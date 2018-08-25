581b2cedbf7dfa7e1161ebcc1e1f57245c4a8d5d
LostTong <tongluohxh@163.com>
2018-08-26 00:10:37 +0800

    ArgsParser
    1. main.cpp中加入args_parser逻辑。
    2.本地测试，log显示正确。

--- MazeProject/MazeProject/src/args_parser.h
+++ MazeProject/MazeProject/src/args_parser.h
@@ -9,13 +9,15 @@ namespace maze {
 		ArgsParser();
 		~ArgsParser();
 
+		void init();
+		bool parse(int argc, char * argv[]);
+
 	public:
 		const std::string load_binary_flag = "--lb";
 		const std::string save_binary_flag = "--sb";
 		const std::string save_svg_flag = "--sv";
 		const std::string generate_maze_flag = "--g";
 
-	private:
 		std::string load_binary_file;
 		std::string save_binary_file;
 		std::string save_svg_file;
@@ -24,10 +26,7 @@ namespace maze {
 		unsigned generate_maze_seed;
 		unsigned generate_maze_width;
 		unsigned generate_maze_height;
-
-	public:
-		void init();
-		bool parse(int argc, char * argv[]);
+		
 	};
 }
 
