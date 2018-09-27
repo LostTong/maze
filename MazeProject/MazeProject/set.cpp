06d33515beefcd2d9c5676051dcc3c0952fd07ae
tongluohxh@163.com <tongluohxh@163.com>
2018-09-27 22:41:40 +0800

    set
    新增remove， last等方法

--- MazeProject/MazeProject/set.cpp
+++ MazeProject/MazeProject/set.cpp
@@ -18,26 +18,33 @@ void Set::add(const ValueType &value) {
 
 template <typename ValueType>
 void Set::add_recursive(const ValueType &value, TreeNode<ValueType> *cur_node) {
-	if (curr == nullptr) {
-		root = new TreeNode<ValueType>(value);
+	// root
+	if (cur_node == nullptr) {
+		cur_node = new TreeNode<ValueType>(value);
+		last = cur_node;
+		size += 1;
 	}
 	else {
-		if (value > cur_node->value) {
-			if (cur_node->right == nullptr) {
-				curr_node->right = new TreeNode<ValueType>(value);
+		// left
+		if (value < cur_node->value) {
+			if(cur_node->left == nullptr){
+				cur_node->left = new TreeNode<ValueType>(value);
+				last = cur_node->left;
+				size += 1;
 			}
-			else {
-				add_rescursive(value, cur_node->right);
+			else{
+				add_rescursive(value, cur_node->left);
 			}
 		}
+		// right
 		else {
-			if (value < cur_node->left) {
-				if (cur_node->left == nullptr) {
-					cur_node->left = TreeNode<ValueType>(value);
-				}
-				else {
-					add_rescursive(value, cur_node->left);
-				}
+			if(cur_node->right == nullptr){
+				cur_node->right = new TreeNode<ValueType>(value);
+				last = cur_node->right;
+				size += 1;
+			}
+			else{
+				add_rescursive(value, cur_node->right);
 			}
 		}
 	}
@@ -53,17 +60,13 @@ bool Set::contain_recursive(const ValueType &value, TreeNode<ValueType> *cur_nod
 	if (cur_node == nullptr) {
 		return false;
 	}
-	else if(value == cur_node->value){
+	if(value == cur_node->value){
 		return true;
 	}
-	else {
-		if (value > curr_node->value) {
-			return contain_recursive(value, cur_node->right);
-		}
-		else {
-			return contain_recursive(value, cur_node->left);
-		}
+	if (value < curr_node->value) {
+		return contain_recursive(value, cur_node->left);
 	}
+	return contain_recursive(value, cur_node->right);
 }
 
 template <typename ValueType>
@@ -72,4 +75,96 @@ void Set::remove(const ValueType &value) {
 }
 
 template <typename ValueType>
-void Set::remove_recursive(const ValueType &value, TreeNode<ValueType> *cur_node, )
+void Set::remove_recursive(const ValueType &value, TreeNode<ValueType> *cur_node, TreeNode<ValueType> *prev_node){
+	if(cur_node != nullptr){
+		if(value == cur_node->value){
+			size -= 1;
+			// left
+			if(cur_node->left != nullptr){
+				// root
+				if(prev_node == nullptr){
+					root = curr->left;
+				}
+				// left
+				else if(prev_node->left == cur_node){
+					prev_node->left = cur_node->left;
+				}
+				// right
+				else{
+					prev_node->right = cur_node->left;
+				}
+
+				if(cur_node->right != nullptr){
+					cur_node->left->right = cur_node->right;
+				}
+			}
+			// right
+			else if(cur_node->right != nullptr){
+				// root
+				if(prev_node == nullptr){
+					root = cur_node->right;
+				}
+				// left
+				else if(prev_node->left == cur_node){
+					prev_node->left = cur_node->right;
+				}
+				// right
+				else{
+					prev_node->right = curr_node->right;
+				}
+			}
+			// leaf
+			else{
+				// root
+				if(prev_node == nullptr){
+					root = nullptr;
+				}
+				// left
+				else if(prev_node->left == curr_node){
+					prev_node->left = nullptr;
+				}
+				// right
+				else{
+					prev_node->right = nullptr;
+				}
+			}
+		}
+		else {
+			if(curr_node->value > value){
+				remove_recursive(value, cur_node->left, cur_node);
+			}
+			else{
+				remove_recursive(value, cur_node->right, cur_node);
+			}
+		}
+	}
+}
+
+template <typename ValueType>
+void Set::clear(){
+	clear_recursive(root);
+}
+
+template <typename ValueType>
+void Set::clear_recursive(TreeNode<ValueType> *cur_node){
+	if(cur_node == nullptr){
+		return;
+	}
+	if(cur_node->left != nullptr){
+		clear_recursive(cur_node->left);
+	}
+	if(cur_node->right != nullptr){
+		clear_recursive(cur_node->right);
+	}
+	delete curr_node;
+}
+
+template <typename ValueType>
+TreeNode<ValueType> *Set::get_last_node(){
+	return last;
+}
+
+template <typename ValueType>
+int Set::get_size(){
+	return size;
+}
--- MazeProject/MazeProject/set.h
+++ MazeProject/MazeProject/set.h
@@ -12,9 +12,16 @@ public:
 	void contains(const ValueType &value) const;
 	void contains_recursive(const ValueType &value, TreeNode<ValueType> *cur_node) const;
 	void remove(const ValueType &value);
+	void remove_recursive(const ValueType &value, TreeNode<ValueType> *cur_node, TreeNode<ValueType> *prev_code);
+	void clear();
+	void clear_recursive(const TreeNode<ValueType> *cur_node);
 	int get_size() const;
+	TreeNode<ValueType> *get_last_node() const;
+
+private:
 	int size;
 	
 	TreeNode<ValueType> *root;
+	TreeNode<ValueType> *last;
 };
 
